3360b75155303ab75aa49445cf3bd798dd2646d7
Ahlam <ahlam20012009@hotmail.fr>
2023-03-13 19:13:31 +0100

    exo

---
+++ exo.js
@@ -0,0 +1,100 @@
+// Réponse1 : la valeur de C sera 24
+//Réponse2 : la valeur de C sera 12312
+// Réponse3 :
+// function price(prixttc){
+// prixttc = parseInt(prompt('Entrez un nombre'));
+// let result = prixttc + prixttc * 0.2;
+// alert (prixttc + result)
+// }
+// price();
+// Réponse4;
+// function nameMoi(nom ,prenom){
+//     nom = (prompt('Entrez votre nom '));
+//     prenom = (prompt('Entrez votre prenom'));
+//  alert('Bonjour ' + prenom + ' votre nom est ' + nom)
+// }
+// nameMoi()
+// Réponse5;
+// function nbr(altr,altr2){
+//     altr =  parseInt(prompt('Entrez un nombre '));
+//     altr2 = parseInt(prompt('Entrez un nombre '));
+//     let result = altr2 * altr ;
+//     if (result < 0){
+//         alert('votre nombre est négatif')
+//     }else if  (result > 0){
+//         alert('votre nombre est positif')
+//     }
+// }
+// nbr()
+// Réponse6;
+// function ut(age){
+//  age =parseInt(prompt('saisir votre age '));
+
+// if (age <13 ) {
+//     alert('Regarder Action man');
+// }else if (age >= 13 && age <=18) {
+//     alert ('Regarder matrix');
+// }
+// else if (age > 18) {
+//     alert('Regarder evil dead')
+// }
+// }
+// ut()
+//Réponse7;
+// function nbr1(){
+//     let random = Math.round(Math.random() *10);
+//     let user;
+//     do{
+//         user = parseInt(prompt('Entrez un nbr en 0 et 10'));
+//     if (user > random){
+//         alert ('Votre nombre est trop grand');
+//     }else if (user < random){
+//         alert ('Votre nombre est trop petit');
+//     }
+//     else if ( user=== random) {
+//         alert ('Vous avez le bon nombre');
+//     }
+//     }while (user != random) 
+// }
+// nbr1()
+//Réponse8;
+// function nbr2(nbr){
+//     nbr = parseInt(prompt('Entrez un nombre'));
+//     for(nbr;nbr >= 0;nbr-- ){
+//     console.log(nbr);
+//     }
+// }
+// nbr2()
+// Réponse9: 
+// function jeu (nbr3){
+//     let random = Math.round(Math.random() *20);
+//     let alm;
+//     do{
+//         alm= parseInt(prompt('Entrez un nbr '));
+//         if (alm > random){
+//                alert ('Votre nombre est trop grand');
+//        }else if (alm < random){
+//               alert ('Votre nombre est trop petit');
+//         } else if ( alm === random) {
+//              alert ('Vous avez le bon nombre');
+//             }
+//     }while (alm != random) 
+// }
+// jeu ()
+//Réponse10:
+// function sum(){
+//      let array = [10, 15, 20, 15, 14, 8]
+//      let somme = 0;
+//       for (let i = 0; i < array.length; i++) {
+//         somme = somme + array[i];
+//     } console.log(somme);
+// }
+// sum();
+// Réponse11;
+function nbrEleve(eleve)
+eleve =parseInt(prompt('Nbrs élèves'));
+
+for (i=0; i< number ; i++){
+    let result = parseInt(prompt('Notes élèves'));
+}
+nbrEleve()
